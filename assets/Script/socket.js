c22c845baa43f788b86a06016eb30e32be02c53e
User3 <user3@iMac2.local>
2017-08-22 09:10:57 +0700

    proto

--- assets/Script/socket.js
+++ assets/Script/socket.js
@@ -31,14 +31,54 @@ socket.Connect = function ()
     // }
 
     //create  socket
-    var url = "ws://45.76.97.239:7000/gamesocket/"+token;
+    var url = "ws://45.76.97.239:9000/gamesocket/"+token;
     // self.ws = new WebSocket(url);
    cc.log("reday to connect "+token);
-    // self.ws.onopen = self.connectionOpen.bind(self);
+     //self.ws.onopen = self.connectionOpen.bind(self);
     // self.ws.onmessage = self.onMessage.bind(self);
     // self.ws.onerror = self.displayError.bind(self);
     // self.ws.onclose = self.socketclose.bind(self);
+
+      self.ws = new WebSocket("ws://45.76.97.239:9000/gamesocket/aa_123123123");
+     self.ws.onopen = function (event) {
+         console.log("Send Text WS was opened."+event); 
+            
+     }; 
+         self.ws.onmessage = function (event) { 
+
+             console.log("response text msg: " + event.data);
+             let protobuf = require("protobufjs");
+             let builder = protobuf.newBuilder();
+             //protobuf.protoFromFile("lobby.proto",builder);
+             protobuf.protoFromFile(cc.url.raw('resources/lobby.proto'),builder);
+             let PB = builder.build('msg');
+           
+             console.log("response text msg: " + typeof(event.data)); 
+              var buf ;
+
+
+             var msgObj = new PB.lobby();
+             msgObj.id = "ss";
+             msgObj.name = "ihowe";
+             msgObj.enterTime ="aa";
+             var buffer = msgObj.toArrayBuffer();
+             let p = PB.lobby.decode(event.data);
+             
+             console.log("response text msg: " + p.id);
+             console.log("response text msg: " + p.name);
+             console.log("response text msg: " + p.enterTime);
+          
+         };
+         self.ws.onerror = function (event) { console.log("Send Text fired an error"); }; 
+         self.ws.onclose = function (event) { console.log("WebSocket instance closed."); };
+        
+          if (self.ws.readyState === WebSocket.OPEN) { 
+                   console.log("Hello WebSocket, I'm a text message."); } 
+             else {
+                   console.log("WebSocket instance wasn't ready..."); } 
 };
+
+
 
 
 socket.connectionOpen = function ()
